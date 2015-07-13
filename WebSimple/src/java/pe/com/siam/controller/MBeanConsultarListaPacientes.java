47700f4e6a4b53213589b26fc68e61d93f4de4fa
Denis Gianpier Huaman <denis07.1993@gmail.com>
2015-07-13 01:21:57 -0500

    Eliminación de método para ver la lista de pacientes de un médico
    Eliminación de método para ver la lista de pacientes de un médico

--- WebSimple/src/java/pe/com/siam/controller/MBeanConsultarListaPacientes.java
+++ WebSimple/src/java/pe/com/siam/controller/MBeanConsultarListaPacientes.java
@@ -75,7 +75,7 @@ public class MBeanConsultarListaPacientes {
         this.fechaProgramada = fechaProgramada;
     }
 
-    public void listarPacienteDeMedicoFecha() {
-        listaPacientes = admClinica.listarPacientesCitaMedico(codMedico, fechaProgramada);
-    }
+//    public void listarPacienteDeMedicoFecha() {
+//        listaPacientes = admClinica.listarPacientesCitaMedico(codMedico, fechaProgramada);
+//    }
 }
